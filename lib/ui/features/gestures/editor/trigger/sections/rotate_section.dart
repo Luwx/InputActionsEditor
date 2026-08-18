f4063a7b0483c2a7c06730bf1b6832a4b214e5c5
Luwx <luwx@hotmail.com.br>
2026-08-18 19:57:37 -0300

    icons for pinch and rotate

--- lib/ui/features/gestures/editor/trigger/sections/rotate_section.dart
+++ lib/ui/features/gestures/editor/trigger/sections/rotate_section.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:forui/forui.dart';
 import 'package:input_actions_editor/model/enums.dart';
 import 'package:input_actions_editor/ui/common/label_with_tooltip.dart';
 import 'package:input_actions_editor/ui/l10n/context_ext.dart';
+import 'package:input_actions_editor/ui/l10n/labels/enum_labels.dart';
 
 class RotateSection extends StatelessWidget {
   const RotateSection({
@@ -14,19 +16,36 @@ class RotateSection extends StatelessWidget {
   final RotateDirection direction;
   final void Function(RotateDirection) onDirectionChanged;
 
+  static const List<RotateDirection> _directions = [
+    RotateDirection.any,
+    RotateDirection.clockwise,
+    RotateDirection.counterclockwise,
+  ];
+
+  static Widget? _icon(RotateDirection direction) => switch (direction) {
+    RotateDirection.any => null,
+    RotateDirection.clockwise => const Icon(FLucideIcons.refreshCw, size: 14),
+    RotateDirection.counterclockwise => const Icon(
+      FLucideIcons.refreshCcw,
+      size: 14,
+    ),
+  };
+
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    final directions = {
-      l10n.directionAny: RotateDirection.any,
-      l10n.directionClockwise: RotateDirection.clockwise,
-      l10n.directionCounterclockwise: RotateDirection.counterclockwise,
-    };
     return SizedBox(
       width: 220,
-      child: FSelect<RotateDirection>(
+      child: FSelect<RotateDirection>.rich(
         key: ValueKey(direction),
-        items: directions,
+        format: (value) => value.label(l10n),
+        prefixBuilder: switch (_icon(direction)) {
+          final icon? => (_, _, _) => Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: icon,
+          ),
+          null => null,
+        },
         control: FSelectManagedControl<RotateDirection>(
           initial: direction,
           onChange: (v) {
@@ -37,6 +56,14 @@ class RotateSection extends StatelessWidget {
           label: l10n.sectionRotateDirectionLabel,
           tooltip: l10n.sectionRotateDirectionTooltip,
         ),
+        children: [
+          for (final value in _directions)
+            FSelectItem<RotateDirection>.item(
+              value: value,
+              title: Text(value.label(l10n)),
+              prefix: _icon(value),
+            ),
+        ],
       ),
     );
   }
